bed0590265fd467dd35e3d850baafd87bac59cc9
koktavy <tavius@nyu.edu>
2022-11-02 00:41:45 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Rubik } from '@next/font/google';
+import './globals.css';
+
+const rubik = Rubik();
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" className={rubik.className}>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <title>Todo App using Next.js 13 + Tigris</title>
+        <meta name="description" content="Tigris app tutorial" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
